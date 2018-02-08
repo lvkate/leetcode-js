dff35b28c27873ebca4cec5279ebc1486f7294ca
Shun Hu <shun.hu@gmail.com>
2018-02-09 00:57:06 +0800

    add 003 lengthOfLongestSubstring and test case

---
+++ src/_003_LongestSubstringWithoutRepeatingCharacters/test.js
@@ -0,0 +1,17 @@
+const lengthOfLongestSubstring = require('./index');
+
+test('lengthOfLongestSubstring function exists', () => {
+    expect(lengthOfLongestSubstring).toBeDefined();
+});
+
+test('Given "abcabcbb", the answer is "abc", which the length is 3', () => {
+    expect(lengthOfLongestSubstring('abcabcbb')).toEqual(3);
+});
+
+test('Given "bbbbb", the answer is "b", with the length of 1', () => {
+    expect(lengthOfLongestSubstring('bbbbb')).toEqual(1);
+});
+
+test('Given "pwwkew", the answer is "wke", with the length of 3', () => {
+    expect(lengthOfLongestSubstring('pwwkew')).toEqual(3);
+});
