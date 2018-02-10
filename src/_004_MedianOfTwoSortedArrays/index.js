c36edb699500d810622249ca388da2225b01f07d
Shun Hu <shun.hu@gmail.com>
2018-02-11 01:23:31 +0800

    add question 004

---
+++ src/_004_MedianOfTwoSortedArrays/index.js
@@ -0,0 +1,35 @@
+/**
+ * Time : O(N) ; Space: O(N)
+ * @tag : Array; Hash Table
+ * @by  : Jason Hu
+ * @date: Feb 11, 2018
+ ***************************************************************************
+ * Description:
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ * Find the median of the two sorted arrays. The overall run time complexity 
+ * should be O(log (m+n)).
+ * 
+ * Example 1:
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 
+ * The median is 2.0
+ * 
+ * Example 2:
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 
+ * The median is (2 + 3)/2 = 2.5
+ ***************************************************************************
+ * {@link https://leetcode.com/problems/median-of-two-sorted-arrays/ }
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    
+};
+module.exports = findMedianSortedArrays;
---
+++ src/_004_MedianOfTwoSortedArrays/test.js
@@ -0,0 +1,14 @@
+const findMedianSortedArrays = require('./index');
+
+test('findMedianSortedArrays function exists', () => {
+    expect(findMedianSortedArrays).toBeDefined();
+});
+
+test('median of [1,3] and [2] is 2.0', () => {
+    expect(findMedianSortedArrays([1,3], [2])).toEqual(2.0);
+});
+
+test('median of [1,2] and [3,4] is 2.5', () => {
+    expect(findMedianSortedArrays([1,2], [3,4])).toEqual(2.5);
+});
+
